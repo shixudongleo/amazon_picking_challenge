e24eb82fa6ed1b1cec0fa1dd7eb37b5ee058d85a
shixudongleo <shixudongleo@gmail.com>
2015-05-26 15:04:45 +0800

    minor modify

--- show_foreground.py
+++ show_foreground.py
@@ -5,8 +5,9 @@
     Usage:
         give a picture with foreground, show foreground
 """
-
+import os
 import cv2
+import numpy as np
 from fg_segmentation.background_subtractor import BackgroundSubtractor
 from argparse import ArgumentParser
 
@@ -17,17 +18,23 @@ if __name__ == '__main__':
     """ usage: python background_subtractor.py -i test.png  """
 
     ap = ArgumentParser()
-    ap.add_argument('-i', '--image', required=True,
+    ap.add_argument('-d', '--directory', required=True,
                     help='the path to the test image')
     args = vars(ap.parse_args())
-    test_img = args['image']
-    test_img = cv2.imread(test_img)
+
+    test_dir = args['directory']
+    test_imgs = [os.path.join(test_dir, file)
+                 for file in os.listdir(test_dir) if file.endswith('.png')]
 
     bg_model = BackgroundSubtractor()
     bg_model = bg_model.build_bg_model()
 
-    fg_mask = bg_model.get_fg_mask(test_img)
+    for file in test_imgs:
+        test_img = cv2.imread(file)
+        fg_mask = bg_model.get_fg_mask(test_img)
+        fg_mask = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2RGB)
 
-    # display foreground image
-    cv2.imshow('foreground', fg_mask)
-    cv2.waitKey(0)
+        display = np.hstack([test_img, fg_mask])
+        # display foreground image
+        cv2.imshow('foreground', display)
+        cv2.waitKey(0)
